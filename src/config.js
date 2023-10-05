bb6638ea59bc817de233d0b15a3838c52b6fd6ca
STING <STING@DESKTOP-HG3C72H>
2023-10-05 18:47:30 -0500

    primer commit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const PORT = process.env.PORT || 4000;
+export const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb+srv://julius:julius3000@cluster0.ktf0np8.mongodb.net/mern-tasks";
+export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
+
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
